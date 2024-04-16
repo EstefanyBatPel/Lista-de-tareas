86da5ca7f7c5834a6f3afad79b4887fb63faf1da
EstefanyBatPel <estefanybatpel@gmail.com>
2024-04-16 10:07:03 +0200

    delete button is working

--- main.js
+++ main.js
@@ -11,5 +11,20 @@ addBtn.addEventListener('click', (e) => {
     const p = document.createElement('p');
     p.textContent = text;
     li.appendChild(p);
+    li.appendChild(addDeleteBtn());
     ul.appendChild(li);
+
+    input.value = "";
 })
+
+function addDeleteBtn() {
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'X';
+    deleteBtn.className = 'btn-delete';
+
+    deleteBtn.addEventListener('click', (e) => {
+        const item = e.target.parentElement;
+        ul.removeChild(item);
+    });
+    return deleteBtn;
+}

4067d7d1ee9c17b561193762bfe52daa334bbd62
EstefanyBatPel <estefanybatpel@gmail.com>
2024-04-16 10:16:44 +0200

    add funtions to hidde predeterminated text

--- main.js
+++ main.js
@@ -6,16 +6,19 @@ const empty = document.querySelector('.empty')
 addBtn.addEventListener('click', (e) => {
     e.preventDefault();
     
-    const text = input.value;
-    const li = document.createElement('li');
-    const p = document.createElement('p');
-    p.textContent = text;
-    li.appendChild(p);
-    li.appendChild(addDeleteBtn());
-    ul.appendChild(li);
+const text = input.value;
+    if (text !== ''){
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.textContent = text;
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
 
-    input.value = "";
-})
+        input.value = "";
+        empty.style.display = 'none';
+    };
+});
 
 function addDeleteBtn() {
     const deleteBtn = document.createElement('button');
@@ -25,6 +28,11 @@ function addDeleteBtn() {
     deleteBtn.addEventListener('click', (e) => {
         const item = e.target.parentElement;
         ul.removeChild(item);
+
+        const items = document.querySelectorAll('li');
+        if (items.length === 0) {
+            empty.style.display = 'block'
+        }
     });
     return deleteBtn;
 }
